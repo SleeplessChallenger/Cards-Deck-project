5729af7d0fdd15426f8e95da1c60e2f2175c395b
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-06 08:49:12 +0500

    Update Cards_Deck_Project.py

--- Cards_Deck_Project.py
+++ Cards_Deck_Project.py
@@ -1,15 +1,16 @@
 from random import shuffle
-class Cards:
 
-	def __init__(self,suit,value):
+
+class Cards:
+	def __init__(self, suit, value):
 		self.suit = suit
 		self.value = value
 
 	def __repr__(self):
 		return f"{self.value} of {self.suit}"
 
-class Decks:
 
+class Decks:
 	def __init__(self):
 		suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 		values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
@@ -22,14 +23,16 @@ class Decks:
 		return f"There are {self.count()} cards in the deck"
 
 	# _ will mean that this method is for internal use only
-	def _dealFirst(self,num):
+	def _dealFirst(self, num):
 		curr = self.count()
 		maxy = min(curr,num)
+		if curr == 0:
+			raise ValueError('No more cards in the pack')
 		try:
 			to_del = self.pack[-maxy:]
 			self.pack = self.pack[:-maxy]
 		except ValueError:
-			return "No more cards in the pack"
+			raise ValueError('No more cards in the pack')
 		return to_del
 
 	def shufflePack(self):
@@ -38,23 +41,23 @@ class Decks:
 		shuffle(self.pack)
 		return self
 
-	#this method will return only one card from the pack dealt
+	# this method will return only one card from the pack dealt
 	def dealSecond(self):
 		to_return = self._dealFirst(1)
 		print(f"This time {to_return[0]} will be removed")
 		return to_return[0]
 
-	#deal the particular number of cards
-	def dealThird(self,num):
+	# deal the particular number of cards
+	def dealThird(self, num):
 		removed = self._dealFirst(num)
 		print(f"There will be {removed} dealt")
 		return removed
 
+
 deck1 = Decks()
 deck1.shufflePack()
 
-#below line will prove that shuffling was successful
+# below line will prove that shuffling was successful
 print(deck1.pack)
 
 deck1.dealSecond()
-
